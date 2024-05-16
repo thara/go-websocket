1aca5d972fc7ed0246eb62fc4a11eefd451831ab
Tomochika Hara <git@thara.dev>
2024-05-16 09:58:46 +0900

    :muscle: Parse opcode

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/base64"
+	"fmt"
 	"io"
 	"log"
 	"log/slog"
@@ -31,7 +32,7 @@ const acceptMagicGUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("Upgrade") != "websocket" && r.Header.Get("Connection") != "Upgrade" {
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
@@ -40,6 +41,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	subprotocols := r.Header.Values("Sec-WebSocket-Protocol")
 	if 0 < len(subprotocols) {
 		w.Header().Set("Sec-WebSocket-Protocol", subprotocols[0])
+		return
+	}
+
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
 		return
 	}
 
@@ -55,4 +62,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Sec-WebSocket-Accept", accept)
 
 	w.WriteHeader(http.StatusSwitchingProtocols)
+
+	conn, buf, err := hj.Hijack()
+	if err != nil {
+		slog.Debug("hijack failed", slog.Any("error", err))
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	b, err := buf.ReadByte()
+	if err != nil {
+		slog.Debug("read failed", slog.Any("error", err))
+		return
+	}
+	op := opcode(b & 0x0F)
+
+	fmt.Printf("opcode: %d\n", op)
 }
+
+type opcode uint8
+
+const (
+	opcodeContinuation opcode = 0x00
+	opcodeText         opcode = 0x01
+	opcodeBinary       opcode = 0x02
+	opcodeClose        opcode = 0x08
+	opcodePing         opcode = 0x09
+	opcodePong         opcode = 0x0A
+)
