5f5d9ef9477e63214b79ec52da4ade6c41ce1411
Tomochika Hara <git@thara.dev>
2024-05-18 22:35:07 +0900

    :muscle: Parse data frame

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha1"
 	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
@@ -71,14 +73,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	b, err := buf.ReadByte()
+	f, err := parseFrame(buf.Reader)
 	if err != nil {
-		slog.Debug("read failed", slog.Any("error", err))
-		return
+		slog.Debug("parseFrame failed", slog.Any("error", err))
+	} else {
+		fmt.Printf("frame: %+v\n", f)
 	}
-	op := opcode(b & 0x0F)
-
-	fmt.Printf("opcode: %d\n", op)
 }
 
 type opcode uint8
@@ -91,3 +91,70 @@ const (
 	opcodePing         opcode = 0x09
 	opcodePong         opcode = 0x0A
 )
+
+type frame struct {
+	final   bool
+	opcode  opcode
+	length  uint64
+	payload []byte
+}
+
+func parseFrame(buf *bufio.Reader) (frame, error) {
+	b, err := buf.ReadByte()
+	if err != nil {
+		return frame{}, fmt.Errorf("read failed: %w", err)
+	}
+	fin := b & 0x80
+	op := opcode(b & 0x0F)
+
+	b, err = buf.ReadByte()
+	if err != nil {
+		return frame{}, fmt.Errorf("read failed: %w", err)
+	}
+	mask := b & 0x80
+	length := uint64(b & 0x7F)
+
+	if length == 126 {
+		var b [2]byte
+		if n, err := buf.Read(b[:]); err != nil {
+			return frame{}, fmt.Errorf("read failed: %w", err)
+		} else if n != 2 {
+			return frame{}, fmt.Errorf("read failed: expected 2 bytes, got %d", n)
+		}
+		length = binary.BigEndian.Uint64(b[:])
+	} else if length == 127 {
+		var b [8]byte
+		if n, err := buf.Read(b[:]); err != nil {
+			return frame{}, fmt.Errorf("read failed: %w", err)
+		} else if n != 8 {
+			return frame{}, fmt.Errorf("read failed: expected 8 bytes, got %d", n)
+		}
+		length = binary.BigEndian.Uint64(b[:])
+	}
+
+	var maskingKey []byte
+	if mask == 0x80 {
+		maskingKey = make([]byte, 4)
+		if n, err := buf.Read(maskingKey); err != nil {
+			return frame{}, fmt.Errorf("read failed: %w", err)
+		} else if n != 4 {
+			return frame{}, fmt.Errorf("read failed: expected 4 bytes, got %d", n)
+		}
+	}
+
+	payload := make([]byte, length)
+	if n, err := buf.Read(payload); err != nil {
+		return frame{}, fmt.Errorf("read failed: %w", err)
+	} else if n != int(length) {
+		return frame{}, fmt.Errorf("read failed: expected %d bytes, got %d", length, n)
+	}
+
+	f := frame{
+		final:   fin == 0x80,
+		opcode:  op,
+		length:  length,
+		payload: payload,
+	}
+
+	return f, nil
+}
